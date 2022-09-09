af4a32fef40390f9550cbfd42ae3eae0be12bd33
DGal02 <dominik.galczyk@gmail.com>
2022-09-09 04:32:22 +0200

    fixed button

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ function Card(props) {
         <div className="card m-3 text-start align-top" style={{width: "18rem", minHeight: "10rem"}}>
             <h5 className="card-header">{props.title}</h5>
             <div className="card-body position-relative">
-                <p className="card-text">{props.description}</p>
+                <p className="card-text pb-4">{props.description}</p>
                 <div className="text-end button-corner">
                     <Button variant="danger" onClick={() => props.handleClick(props.index)}>
                         Delete
